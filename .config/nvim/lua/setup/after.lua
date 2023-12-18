46f438ee78d491609f368e2268fff6577069d2c2
Szarvas Péter <peterszarvas94@gmail.com>
2023-12-18 15:12:35 +0100

    feat: add support for go templ

--- .config/nvim/lua/setup/after.lua
+++ .config/nvim/lua/setup/after.lua
@@ -25,25 +25,62 @@ require('which-key').register {
 
 require('colorizer').setup({
   user_default_options = {
-    RGB = true,               -- #RGB hex codes
-    RRGGBB = true,            -- #RRGGBB hex codes
-    names = false,             -- "Name" codes like Blue or blue
-    RRGGBBAA = true,          -- #RRGGBBAA hex codes
-    AARRGGBB = true,          -- 0xAARRGGBB hex codes
-    rgb_fn = true,            -- CSS rgb() and rgba() functions
-    hsl_fn = true,            -- CSS hsl() and hsla() functions
-    css = true,               -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = true,            -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    RGB = true,          -- #RGB hex codes
+    RRGGBB = true,       -- #RRGGBB hex codes
+    names = false,       -- "Name" codes like Blue or blue
+    RRGGBBAA = true,     -- #RRGGBBAA hex codes
+    AARRGGBB = true,     -- 0xAARRGGBB hex codes
+    rgb_fn = true,       -- CSS rgb() and rgba() functions
+    hsl_fn = true,       -- CSS hsl() and hsla() functions
+    css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    css_fn = true,       -- Enable all CSS *functions*: rgb_fn, hsl_fn
     -- Available modes for `mode`: foreground, background,  virtualtext
-    mode = "background",     -- Set the display mode.
+    mode = "background", -- Set the display mode.
     -- Available methods are false / true / "normal" / "lsp" / "both"
     -- True is same as normal
-    tailwind = true,                                    -- Enable tailwind colors
+    tailwind = true,                                 -- Enable tailwind colors
     -- parsers can contain values used in |user_default_options|
-    sass = { enable = false, parsers = { "css" }, },     -- Enable sass colors
+    sass = { enable = false, parsers = { "css" }, }, -- Enable sass colors
     virtualtext = "■",
     -- update color values even if buffer is not focused
     -- example use: cmp_menu, cmp_docs
     always_update = false
   },
 })
+
+vim.filetype.add({
+  extension = {
+    templ = "templ",
+  },
+})
+
+---@diagnostic disable-next-line: missing-fields
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "go",
+    "javascript",
+    "typescript",
+    "html",
+    "css",
+    "json",
+    "yaml",
+    "lua",
+    "rust",
+    "toml",
+    "markdown",
+    "jsdoc",
+    "bash",
+  },
+  highlight = {
+    enable = true,
+  },
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.templ = {
+  install_info = {
+    url = "https://github.com/vrischmann/tree-sitter-templ",
+    files = { "src/parser.c", "src/scanner.c" },
+  },
+  filetype = "templ",
+}
