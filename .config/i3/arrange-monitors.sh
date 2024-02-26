01d1f7b492227e193283408aee708101133a4b22
Szarvas Péter <contact@peterszarvas.hu>
2024-02-26 12:10:01 +0100

    feat: better handling of monitor

--- .config/i3/arrange-monitors.sh
+++ .config/i3/arrange-monitors.sh
@@ -3,11 +3,31 @@
 big="HDMI-1"
 small="eDP-1"
 
-if xrandr --query | grep "$big connected"; then
+# check if the big monitor is connected
+big_connected=$(xrandr --query | grep "$big connected")
+
+# if big is not connected, quit
+if [ -z "$big_connected" ]; then
+  exit 0
+fi
+
+# check if big is active
+big_is_active=$(xrandr --listactivemonitors | grep "$big" | wc -l)
+echo $big_is_active
+
+# check if small is active
+small_is_active=$(xrandr --listactivemonitors | grep "$small" | wc -l)
+echo $small_is_active
+
+# 3 stage: only big, only small, both
+if [ $big_is_active -eq 1 ] && [ $small_is_active -eq 1 ]; then
+  xrandr --output $big --primary --mode 2560x1080 --pos 0x0 --rotate normal --output $small --off
+  polybar -r big
+elif [ $big_is_active -eq 1 ] && [ $small_is_active -eq 0 ]; then
+  xrandr --output $small --mode 1920x1080 --pos 0x0 --rotate normal --output $big --off
+  polybar -r small
+elif [ $big_is_active -eq 0 ] && [ $small_is_active -eq 1 ]; then
   xrandr --output $big --primary --mode 2560x1080 --pos 0x0 --rotate normal --output $small --mode 1920x1080 --pos 320x1080 --rotate normal
-  # xrandr --output $big --primary --mode 2560x1080 --pos 0x0 --rotate normal --output $small --off
-else
-  if xrandr --query | grep "$small connected"; then
-    xrandr --output $small --mode 1920x1080 --pos 0x0 --rotate normal --output $big --off
-  fi
+  polybar -r big
+  polybar -r small
 fi
