cf420c2c222995138490abb9ea6daef8f0c77287
Péter Szarvas <contact@peterszarvas.hu>
2024-02-28 16:24:33 +0100

    change: setup script
    1.1

--- wsl_ubuntu_setup.sh
+++ wsl_ubuntu_setup.sh
@@ -8,32 +8,10 @@ sudo apt install build-essential
 sudo apt install unzip 
 sudo apt install fzf 
 sudo apt install ripgrep
-sudo apt install zsh
-sudo apt install tmux
 sudo api install xclip
 
-# clone dots
-mkdir ~/projects
-cd ~/projects
-git clone https://github.com/peterszarvas94/dots
-
-# set up zsh
-cd ~
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-rm ~/.zshrc
-ln -s ~/projects/dots/.zshrc ~/.zshrc
-
-# set up neovim
-mkdir -p ~/Downloads
-cd ~/Downloads
-wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
-sudo mv ~/Downloads/nvim-linux64 /usr/lib/
-sudo ln -s /usr/lib/nvim-linux64/bin/nvim /usr/bin/nvim
-rm -rf ~/Downloads/*
-mkdir -p ~/.config
-ln -s ~/projects/dots/.config/nvim ~/.config/nvim
+# hide welcome message
+sudo touch /root/.hushlogin
 
 # cmake
 cd ~/Downloads
@@ -51,22 +29,40 @@ sudo mv ~/Downloads/go /usr/lib
 sudo ln -s /usr/lib/go/bin/go /usr/bin
 rm -rf ~/Downloads/*
 
-# nvm, node, npm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+# clone dots
+mkdir ~/projects
+cd ~/projects
+git clone https://github.com/peterszarvas94/dots
+cd ~/projects/dots
+git remote remove origin
+git remote add origin git@github.com:peterszarvas94/dots.git
 
-# link config files
+# set up neovim
+mkdir -p ~/Downloads
+cd ~/Downloads
+wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+sudo mv ~/Downloads/nvim-linux64 /usr/lib/
+sudo ln -s /usr/lib/nvim-linux64/bin/nvim /usr/bin/nvim
+rm -rf ~/Downloads/*
+mkdir -p ~/.config
+ln -s ~/projects/dots/.config/nvim ~/.config/nvim
+
+# gitconfig
 rm -rf ~/.gitconfig
 ln -s ~/projects/dots/.gitconfig.personal ~/.gitconfig
 # ln -s ~/projects/dots/.gitconfig.work ~/.gitconfig
 
+# gitignore
 rm -rf ~/.gitignore
 ln -s ~/projects/dots/.gitignore ~/.gitignore
 
+# tmux
+sudo apt install tmux
 rm -rf ~/.tmux.conf
 ln -s ~/projects/dots/.tmux.conf ~/.tmux.conf
 
+# scripts
 mkdir -p ~/.local/bin
 ln -s ~/projects/dots/.local/bin/get-keycode ~/.local/bin/get-keycode
 ln -s ~/projects/dots/.local/bin/killport ~/.local/bin/killport
@@ -74,21 +70,24 @@ ln -s ~/projects/dots/.local/bin/light ~/.local/bin/light
 ln -s ~/projects/dots/.local/bin/stopwatch ~/.local/bin/stopwatch
 ln -s ~/projects/dots/.local/bin/tmux-sessionizer ~/.local/bin/tmux-sessionizer
 
-# tailwind cli
-npm i -g tailwindcss
-
-# ssh-key
+# ssh
 mkdir -p ~/.ssh
 cd ~/.ssh
 echo "github" | ssh-keygen -t ed25519 -C "contact@peterszarvas.hu"
 cat ~/.ssh/github.pub | xclip -selection clipboard
 cmd.exe /c start chrome https://github.com/settings/ssh/new
 
-# change dotfiles git origin
-cd ~/projects/dots
-git remote remove origin
-git remote add origin git@github.com:peterszarvas94/dots.git
+# nvm, node, npm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
-# restart zsh
+# tailwind cli
+npm i -g tailwindcss
+
+# set up zsh
 cd ~
-exec zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm ~/.zshrc
+ln -s ~/projects/dots/.zshrc ~/.zshrc
