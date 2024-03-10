c3fb0b11b2a5786e960e7af237abd51bc1a2e2ec
Péter Szarvas <contact@peterszarvas.hu>
2024-03-10 17:54:16 +0100

    fix: monitor layout

--- .config/i3/arrange-monitors.sh
+++ .config/i3/arrange-monitors.sh
@@ -23,11 +23,14 @@ echo $small_is_active
 if [ $big_is_active -eq 1 ] && [ $small_is_active -eq 1 ]; then
   xrandr --output $big --primary --mode 2560x1080 --pos 0x0 --rotate normal --output $small --off
   polybar -r big
+  feh --bg-fill ~/projects/dots/background/bing_wallpaper_MWDolomites_1920x1080.jpg
 elif [ $big_is_active -eq 1 ] && [ $small_is_active -eq 0 ]; then
   xrandr --output $small --mode 1920x1080 --pos 0x0 --rotate normal --output $big --off
   polybar -r small
+  feh --bg-fill ~/projects/dots/background/city-1679690962777-4742.jpg
 elif [ $big_is_active -eq 0 ] && [ $small_is_active -eq 1 ]; then
-  xrandr --output $big --primary --mode 2560x1080 --pos 0x0 --rotate normal --output $small --mode 1920x1080 --pos 320x1080 --rotate normal
+  xrandr --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output eDP-1 --mode 1920x1080 --pos 320x1080 --rotate normal
   polybar -r big
   polybar -r small
+  feh --bg-fill ~/projects/dots/background/bing_wallpaper_MWDolomites_1920x1080.jpg --bg-fill ~/projects/dots/background/city-1679690962777-4742.jpg
 fi
