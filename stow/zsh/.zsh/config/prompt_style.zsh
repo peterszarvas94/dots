fcfcba8231419c880290d0dfb92eda8ac9d5af46
Péter Szarvas <contact@peterszarvas.hu>
2025-02-07 13:54:42 +0100

    fix: zsh git branch name

--- stow/zsh/.zsh/config/prompt_style.zsh
+++ stow/zsh/.zsh/config/prompt_style.zsh
@@ -1,5 +1,5 @@
 function git_branch_name() {
-  branch=$(git symbolic-ref HEAD 2>/dev/null | awk -F/ '{print $NF}')
+  branch=$(git rev-parse --abbrev-ref HEAD)
   if [[ $branch == "" ]]; then
     :
   else
