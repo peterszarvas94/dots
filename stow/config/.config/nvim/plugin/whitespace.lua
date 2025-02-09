ae544730c31ff38eee74e4a85e6346f1b825e609
Péter Szarvas <contact@peterszarvas.hu>
2025-02-09 20:37:25 +0100

    feat: 4 space indent

--- stow/config/.config/nvim/plugin/whitespace.lua
+++ stow/config/.config/nvim/plugin/whitespace.lua
@@ -1,6 +1,6 @@
 -- set spaces as tabs
 local function spaces()
-  vim.cmd 'set tabstop=2 | set shiftwidth=2 | set expandtab'
+  vim.cmd 'set tabstop=4 | set shiftwidth=4 | set expandtab'
   vim.cmd [[ %s/\t/  /ge | update ]]
 end
 
@@ -8,7 +8,7 @@ vim.api.nvim_create_user_command('Spaces', spaces, {})
 
 -- set tabs as tabs
 local function tabs()
-  vim.cmd 'set tabstop=2 | set shiftwidth=2 | set noexpandtab'
+  vim.cmd 'set tabstop=4 | set shiftwidth=4 | set noexpandtab'
   vim.cmd [[ %s/  /\t/ge | update ]]
 end
 
