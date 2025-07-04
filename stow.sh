f32eaedb71c3c74d719f1208188ef2cea7c447f0
Peter Szarvas <contact@peterszarvas.hu>
2025-07-04 14:50:13 +0200

    separate platforms

--- stow.sh
+++ stow.sh
@@ -20,11 +20,11 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Common packages for all platforms
-COMMON="scripts tmux zsh aerospace alacritty git nvim opencode starship zed ghostty aerc"
+COMMON="scripts tmux zsh alacritty git nvim opencode zed ghostty aerc"
 
 # Only run if platform is specified
 if [[ "$PLATFORM" == "mac" ]]; then
-    stow --dir=stow --target=$HOME $COMMON skhd yabai amethyst karabiner
+    stow --dir=stow --target=$HOME $COMMON zsh-mac aerospace
 elif [[ "$PLATFORM" == "arch" ]]; then
     stow --dir=stow --target=$HOME $COMMON hypr waybar
 else
