edfc832867305d2c5449b8287cf60de317e1a182
Peter Szarvas <contact@peterszarvas.hu>
2026-01-06 10:32:08 +0100

    embrace defaults

--- stow/neovim/.config/nvim/lua/config/options.lua
+++ stow/neovim/.config/nvim/lua/config/options.lua
@@ -5,8 +5,8 @@ vim.opt.belloff = ''
 -- vim.opt.winblend = 20 -- 0-100, where 100 is fully transparent
 -- vim.opt.pumblend = 20 -- same for popup menus
 
-vim.o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50'
--- vim.o.guicursor = 'n-v-c:block'
+-- vim.o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50'
+vim.o.guicursor = 'n-v-c:block'
 vim.o.hlsearch = false
 vim.o.number = true
 vim.o.relativenumber = false
--- stow/zsh/.zsh/config/cursor.zsh
+++ stow/zsh/.zsh/config/cursor.zsh
@@ -1,22 +1,22 @@
 # Change cursor shape for different vi modes
-function zle-keymap-select {
-  if [[ ${KEYMAP} == vicmd ]] ||
-     [[ $1 = 'block' ]]; then
-    echo -ne '\e[2 q' # Block cursor, "/e[1 q is blinking"
-  elif [[ ${KEYMAP} == main ]] ||
-       [[ ${KEYMAP} == viins ]] ||
-       [[ ${KEYMAP} = '' ]] ||
-       [[ $1 = 'beam' ]]; then
-    echo -ne '\e[6 q' # Beam cursor "/e[5 q is blinking
-  fi
-}
-zle -N zle-keymap-select
-
-# Initial cursor shape
-echo -ne '\e[6 q'
-
-# Reset cursor when starting new terminal
-function zle-line-init() {
-    echo -ne '\e[6 q'
-}
-zle -N zle-line-init
+# function zle-keymap-select {
+#   if [[ ${KEYMAP} == vicmd ]] ||
+#      [[ $1 = 'block' ]]; then
+#     echo -ne '\e[2 q' # Block cursor, "/e[1 q is blinking"
+#   elif [[ ${KEYMAP} == main ]] ||
+#        [[ ${KEYMAP} == viins ]] ||
+#        [[ ${KEYMAP} = '' ]] ||
+#        [[ $1 = 'beam' ]]; then
+#     echo -ne '\e[6 q' # Beam cursor "/e[5 q is blinking
+#   fi
+# }
+# zle -N zle-keymap-select
+#
+# # Initial cursor shape
+# echo -ne '\e[6 q'
+#
+# # Reset cursor when starting new terminal
+# function zle-line-init() {
+#     echo -ne '\e[6 q'
+# }
+# zle -N zle-line-init
