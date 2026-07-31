b301db70a862481a546bca851d2378f396f4a958
Peter Szarvas <contact@peterszarvas.hu>
2026-07-31 12:26:47 +0200

    revert organize-imports plugin

--- stow/nvim/.config/nvim/lua/config/options.lua
+++ stow/nvim/.config/nvim/lua/config/options.lua
@@ -14,4 +14,11 @@ else
   require("config.remote_clipboard").setup()
 end
 vim.opt.relativenumber = false
+
+-- Keep swap files in a user-owned directory. The old directory was created as
+-- root and causes E303 when opening files.
+local swap_dir = vim.fn.stdpath("state") .. "/swap-peti"
+vim.fn.mkdir(swap_dir, "p", 448) -- 0700
+vim.opt.directory = swap_dir .. "//"
+
 vim.g.autoformat = true
