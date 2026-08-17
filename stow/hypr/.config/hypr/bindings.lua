ff0decac88a5c42d4e231f082bdb8501e6eed1a0
Peter Szarvas <contact@peterszarvas.hu>
2026-08-17 15:02:44 +0200

    remove binding

--- stow/hypr/.config/hypr/bindings.lua
+++ stow/hypr/.config/hypr/bindings.lua
@@ -1,3 +1,6 @@
+-- Disable Omarchy's workspace layout toggle.
+hl.unbind("SUPER + L")
+
 -- Notification shortcuts that replace Omarchy's comma bindings.
 o.bind("SUPER + N", "Dismiss last notification", "omarchy-shell notifications dismissOne")
 o.bind("SUPER + ALT + N", "Invoke last notification", "omarchy-shell notifications invokeLast")
